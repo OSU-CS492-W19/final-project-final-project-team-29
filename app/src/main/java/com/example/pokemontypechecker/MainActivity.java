6dbecbc2ee4dfc10b1ced322b612a2022f0a3253
Daniel Grocki <grockid@oregonstate.edu>
2019-03-02 13:52:12 -0800

    types view set up

--- app/src/main/java/com/example/pokemontypechecker/MainActivity.java
+++ app/src/main/java/com/example/pokemontypechecker/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.pokemontypechecker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,14 +17,26 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements TypeAdapter.OnSearchItemClickListener,NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        RecyclerView mtypeView = findViewById(R.id.rv_types_list);
+
+        TypeAdapter mTypeAdapter = new TypeAdapter(this);
+        mtypeView.setAdapter(mTypeAdapter);
+        mtypeView.setLayoutManager(new LinearLayoutManager(this));
+        mtypeView.setHasFixedSize(true);
+
+
+
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
@@ -41,7 +56,42 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+        List<String> types  = new ArrayList<String>();
+        types.add("fire");
+        types.add("water");
+        types.add("poison");
+        types.add("fire");
+        types.add("fire");
+        types.add("fire");
+        types.add("fire");
+        types.add("fire");
+        types.add("fire");
+        types.add("fire");
+        types.add("fire");
+        types.add("fire");
+        types.add("fire");
+        types.add("fire");
+        types.add("fire");
+        types.add("fire");
+        types.add("fire");
+        types.add("fire");
+        types.add("fire");
+
+
+
+
+        mTypeAdapter.updateSearchResults(types);
     }
+
+    @Override
+    public void onSearchItemClick(String s) {
+        //Intent intent = new Intent(this, ForecastItemDetailActivity.class);
+        //intent.putExtra(OpenWeatherMapUtils.EXTRA_FORECAST_ITEM, forecastItem);
+        //startActivity(intent);
+    }
+
 
     @Override
     public void onBackPressed() {
