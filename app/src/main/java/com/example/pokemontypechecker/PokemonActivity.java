062dc71b9a595f8aa1d44241c1f51d4c624bfd16
danvanhorn <dan.vanhorn17@gmail.com>
2019-03-08 18:06:47 -0800

    testing add to db

--- app/src/main/java/com/example/pokemontypechecker/PokemonActivity.java
+++ app/src/main/java/com/example/pokemontypechecker/PokemonActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.TextView;
 
+import com.example.pokemontypechecker.data.Pokemon;
 import com.example.pokemontypechecker.utils.PokemonUtils;
 
 import java.util.ArrayList;
@@ -22,26 +23,7 @@ public class PokemonActivity extends AppCompatActivity implements PokemonAdapter
     private String mPokemonType;
 
     // TODO: will remove this, temporary list of pokemon names
-    private List<String> names = new ArrayList<String>() {{
-        add("Pikachu");
-        add("Geodude");
-        add("Gyrados");
-        add("Magikarp");
-        add("Rattata");
-        add("Pidgey");
-        add("Castform");
-        add("Wailmer");
-        add("Blaziken");
-        add("Mudkip");
-        add("Seedot");
-        add("Oddish");
-        add("Combusken");
-        add("Torchic");
-        add("Goldeen");
-        add("Seadra");
-        add("Raichu");
-        add("Shuckle");
-    }};
+    private List<Pokemon> mPokemon = null;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -67,20 +49,48 @@ public class PokemonActivity extends AppCompatActivity implements PokemonAdapter
             mPokemonType = intent.getStringExtra(PokemonUtils.POKEMON_TYPE);
         }
 
+        mPokemon = new ArrayList<Pokemon>() {{
+            add(new Pokemon() {{
+                id = 1;
+                name = "Pikachu";
+                url = "https://pokemon.com/1";
+            }});
+            add(new Pokemon() {{
+                id = 2;
+                name = "Geodude";
+                url = "https://pokemon.com/2";
+            }});
+            add(new Pokemon() {{
+                id = 3;
+                name = "Gyrados";
+                url = "https://pokemon.com/3";
+            }});
+            add(new Pokemon() {{
+                id = 4;
+                name = "Magikarp";
+                url = "https://pokemon.com/4";
+            }});
+            add(new Pokemon() {{
+                id = 5;
+                name = "Rattata";
+                url = "https://pokemon.com/5";
+            }});
+        }};
+
         mPokemonHeaderTV.setText(mPokemonType);
-        mPokemonAdapter.updatePokemonNameResults(names);
+        mPokemonAdapter.updatePokemonResults(mPokemon);
     }
 
     @Override
-    public void onPokemonClick(String pokemon) {
+    public void onPokemonClick(Pokemon pokemon) {
         Intent intent = new Intent(this, PokemonDetailsActivity.class);
         intent.putExtra(PokemonUtils.POKEMON_NAME, pokemon);
         startActivity(intent);
     }
 
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        outState.putString(PokemonUtils.POKEMON_TYPE, mPokemonType);
-        super.onSaveInstanceState(outState);
-    }
+//    @Override
+//    protected void onSaveInstanceState(Bundle outState) {
+//        outState.putString(PokemonUtils.POKEMON_TYPE, mPokemonType);
+//        super.onSaveInstanceState(outState);
+//    }
 }
--- app/src/main/java/com/example/pokemontypechecker/data/Pokemon.java
+++ app/src/main/java/com/example/pokemontypechecker/data/Pokemon.java
@@ -3,9 +3,10 @@ package com.example.pokemontypechecker.data;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
 import android.support.annotation.NonNull;
+import java.io.Serializable;
 
 @Entity(tableName = "pokemon")
-public class Pokemon {
+public class Pokemon implements Serializable {
     @NonNull
     @PrimaryKey
     public int id;
