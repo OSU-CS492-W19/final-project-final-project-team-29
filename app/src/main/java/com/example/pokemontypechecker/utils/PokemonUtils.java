4afb2fbfacc96cd244ccdd5bab6736e5b3ba8d3c
Dan Van Horn <dan.vanhorn17@gmail.com>
2019-03-14 17:49:06 -0700

    use different key values

--- app/src/main/java/com/example/pokemontypechecker/utils/PokemonUtils.java
+++ app/src/main/java/com/example/pokemontypechecker/utils/PokemonUtils.java
@@ -3,5 +3,5 @@ package com.example.pokemontypechecker.utils;
 public class PokemonUtils {
     public static final String POKEMON_NAME = "pokemon_name";
     public static final String POKEMON_TYPE = "pokemon_type";
-    public static final String POKEMON_URL = "pokemon_type";
+    public static final String POKEMON_URL = "pokemon_url";
 }
